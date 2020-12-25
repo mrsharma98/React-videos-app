1c4050ab359828d2e0c8abdb118d9a0359670848
Manish Sharma <mrsharma98@gmail.com>
2020-12-26 01:06:37 +0530

    included Video components and fetched the items

---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import VideoItem from './VideoItem'
+
+const VideoList = ({ videos }) => {
+
+    const renderList = videos.map((video) => {
+        return <VideoItem video={video} />
+    })
+
+    return <div>{renderList}</div>
+}
+
+export default VideoList
