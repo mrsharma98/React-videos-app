2be3debb6d3f894047e2fcc8388e248aff97be72
Manish Sharma <mrsharma98@gmail.com>
2020-12-25 23:06:10 +0530

    Started the project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                Hello World
+                <SearchBar />
+            </div>
+        )
+    }
+}
+
+export default App
